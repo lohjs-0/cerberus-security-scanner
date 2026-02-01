4698e2ed498b1ffdd288609df1ad72bf59700526
Lohane Massão <massaolohane7@gmail.com>
2026-01-31 22:57:56 -0300

    Add insecure HTTP methods scanner

---
+++ cerberus/src/scanners/methods.scanner.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+
+exports.checkMethods = async (targetUrl) => {
+  try {
+    const dangerousMethods = ["PUT", "DELETE", "PATCH"];
+    const allowed = [];
+
+    for (const method of dangerousMethods) {
+      try {
+        const response = await axios({
+          method,
+          url: targetUrl
+        });
+
+        if (response.status < 500) {
+          allowed.push(method);
+        }
+      } catch (err) {
+        // ignorar erro
+      }
+    }
+
+    if (allowed.length > 0) {
+      return {
+        vulnerability: "Insecure HTTP Methods",
+        severity: "MEDIUM",
+        description: `Métodos inseguros habilitados: ${allowed.join(", ")}`
+      };
+    }
+  } catch (error) {
+    return null;
+  }
+};
--- cerberus/src/services/scan.service.js
+++ cerberus/src/services/scan.service.js
@@ -1,3 +1,4 @@
+const methodsScanner = require("../scanners/methods.scanner");
 const authScanner = require("../scanners/auth.scanner");
 const sqliScanner = require("../scanners/sqli.scanner");
 const headersScanner = require("../scanners/headers.scanner");
@@ -17,6 +18,9 @@ exports.executeScan = async (targetUrl) => {
 
   const xssResult = await xssScanner.checkXSS(targetUrl);
   if (xssResult) vulnerabilities.push(xssResult);
+
+  const methodsResult = await methodsScanner.checkMethods(targetUrl);
+if (methodsResult) vulnerabilities.push(methodsResult);
 
   return {
     target: targetUrl,
